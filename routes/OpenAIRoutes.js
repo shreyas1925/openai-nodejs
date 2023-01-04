8af3002e7ff8081cb7446b4fd89c1ba204ebd37b
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-01-04 19:28:47 +0530

    changed

--- routes/OpenAIRoutes.js
+++ routes/OpenAIRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const generateImage = require('../controllers/OpenAIController');
+const { generateImage } = require('../controllers/OpenAIController');
 const router = express.Router();
 
 router.post('/generateImage', generateImage)
